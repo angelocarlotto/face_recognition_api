4994dee789a22eb8b43cb499706c66d57f608c35
Angelo Carlotto <angelocarlotto@gmail.com>
2024-08-08 10:53:17 -0400

    update

--- api/main.py
+++ api/main.py
@@ -17,7 +17,7 @@ CORS(app)
 known_faces={}
 @app.route("/api/hi",methods=["GET"])
 def hi():
-    return  jsonify("i am allive"), 200
+    return  jsonify(f"i am allive:{os.path}"), 200
 
 @app.route("/api/save",methods=["GET"])
 def save():
